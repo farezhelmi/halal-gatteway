28fb8df1a2c3ec1b7b1b809849599cf3938a0ee8
farezhelmi <141869230+farezhelmi@users.noreply.github.com>
2024-10-09 03:05:00 +0800

    update service provider and middleware

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,9 +24,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         // Force HTTPS in production
-        if ($this->app->environment('production')) {
-            URL::forceScheme('https');
-        }
+        // if ($this->app->environment('production')) {
+        //     URL::forceScheme('https');
+        // }
         //compose all the views....
         view()->composer('*', function ($view) 
         {
